b880da14cefae7792b149c829ea4ca03683aff5d
Janol96 <janhandke10@gmail.com>
2025-06-11 18:59:39 +0200

    Komenatrze Game.h

--- include/Game.h
+++ include/Game.h
@@ -1,16 +1,22 @@
 #pragma once
+
 #include "Player.h"
 #include "AIPlayer.h"
 
+// Klasa Game zarządza rozgrywką między graczem a AI
 class Game {
 private:
-    Player& human;
-    AIPlayer ai;
-    bool playerTurn;
+    Player& human;     // Referencja do gracza (człowieka)
+    AIPlayer ai;       // Obiekt przeciwnika sterowanego przez komputer
+    bool playerTurn;   // Flaga określająca, czy teraz jest tura gracza
 
 public:
+    // Konstruktor - inicjalizuje grę z podanym graczem
     Game(Player& humanPlayer);
 
+    // Główna pętla gry (obsługuje logikę i zdarzenia)
     void run(sf::RenderWindow& window);
+
+    // Rysuje obie plansze: gracza i AI (z przesunięciem i skalą)
     void drawBoards(sf::RenderWindow& window, float tileSize, sf::Vector2f offsetPlayer, sf::Vector2f offsetAI);
 };
